8727b6400f94551322216ecfd50e733c25186168
Andres Castellanos <andrescaste29@gmail.com>
2018-02-16 19:13:10 -0600

    Primera refactorización

--- src/Source/TaxCalculator.java
+++ src/Source/TaxCalculator.java
@@ -5,6 +5,10 @@ public class TaxCalculator {
     private static final double MINIMUMSALARY = 88.36;
     
    public double calculateIncomeTax(Worker worker){
-       return 0;    
+       if(worker.getHoursworked() > 96){
+           return 1;
+       } else {
+           return 0;
+       } 
    }
 }
